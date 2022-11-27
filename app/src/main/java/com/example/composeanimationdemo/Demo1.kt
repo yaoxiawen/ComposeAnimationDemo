3234879fb421986b695995e7c5874295b8ff8166
yaoxiawen <735649296@qq.com>
2022-11-27 16:29:09 +0800

    手势动画，滑动删除

--- app/src/main/java/com/example/composeanimationdemo/Demo1.kt
+++ app/src/main/java/com/example/composeanimationdemo/Demo1.kt
@@ -4,13 +4,27 @@ import androidx.compose.animation.*
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.horizontalDrag
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.consumePositionChange
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.input.pointer.util.VelocityTracker
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
 
 /**
  * 简单值动画, animateColorAsState, 另外还有很多以animate开头作为状态AsState的动画
@@ -197,3 +211,91 @@ fun Demo7() {
         )
     }
 }
+
+/**
+ * 手势动画，滑动删除
+ */
+@Composable
+fun Demo8() {
+    Text(text = "滑动删除",
+        modifier = Modifier
+            .swipeToDismiss { }
+            .fillMaxWidth()
+            .height(50.dp)
+            .background(Color.LightGray)
+            //文字竖直居中
+            .wrapContentHeight(Alignment.CenterVertically)
+    )
+}
+
+fun Modifier.swipeToDismiss(onDismissed: () -> Unit): Modifier = composed {
+    composed {
+        //水平位移
+        val offsetX = remember {
+            Animatable(0f)
+        }
+        //使用 pointerInput 修饰符，可以获取对传入指针触摸事件的低级别访问，并跟踪用户使用同一指针拖动的速度。
+        pointerInput(Unit) {
+            //衰减动画，用于计算滑动动画的最终停止位置
+            val decay = splineBasedDecay<Float>(this)
+            //开启协程监听触摸事件
+            coroutineScope {
+                //使用while (true)一直监听触摸事件
+                while (true) {
+                    //等待触摸按下事件
+                    //awaitPointerEventScope：挂起并安装指针输入块，该块可以等待输入事件并立即响应它们
+                    //awaitFirstDown：第一个down事件
+                    val pointerId = awaitPointerEventScope {
+                        awaitFirstDown().id
+                    }
+                    //如果动画当前正在运行，我们应将其拦截。可以通过对 Animatable 调用 stop 来实现此目的。
+                    // 请注意，如果动画未运行，系统会忽略该调用。VelocityTracker 用于计算用户从左向右移动的速度。
+                    offsetX.stop()
+                    val velocityTracker = VelocityTracker()
+                    //等待拖动事件
+                    awaitPointerEventScope {
+                        //监听水平滑动
+                        horizontalDrag(pointerId) { change ->
+                            val horizontalDragOffset = offsetX.value + change.positionChange().x
+                            //只能在另一个 launch 代码块内发起对 snapTo 的调用，
+                            // 因为 awaitPointerEventScope 和 horizontalDrag 是受限的协程范围。
+                            // 也就是说，它们只能对 awaitPointerEvents 执行 suspend，而 snapTo 并不是指针事件。
+                            launch {
+                                offsetX.snapTo(horizontalDragOffset)
+                            }
+                            //记录下时间和位置就可以计算出速度
+                            velocityTracker.addPosition(change.uptimeMillis, change.position)
+                            //消费掉手势事件，而不是传递给外部
+                            change.consumePositionChange()
+                        }
+                    }
+                    //滑动事件结束，计算滑动速度
+                    val velocity = velocityTracker.calculateVelocity().x
+                    //计算最终停止位置
+                    val targetOffsetX = decay.calculateTargetValue(offsetX.value, velocity)
+                    //给动画设置边界
+                    offsetX.updateBounds(
+                        lowerBound = -size.width.toFloat(),
+                        upperBound = size.width.toFloat()
+                    )
+                    launch {
+                        if (targetOffsetX.absoluteValue <= size.width) {
+                            //返回原位
+                            offsetX.animateTo(targetValue = 0f, initialVelocity = velocity)
+                        } else {
+                            //使用衰减动画直到滑动边界
+                            offsetX.animateDecay(velocity, decay)
+                            onDismissed()
+                        }
+                    }
+                }
+            }
+        }
+            //应用水平位移给元素
+            .offset { IntOffset(offsetX.value.roundToInt(), 0) }
+    }
+}
+
+
+
+
--- app/src/main/java/com/example/composeanimationdemo/MainActivity.kt
+++ app/src/main/java/com/example/composeanimationdemo/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeAnimationDemoTheme {
-                Demo7()
+                Demo8()
             }
         }
     }
@@ -29,6 +29,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     ComposeAnimationDemoTheme {
-        Demo7()
+        Demo8()
     }
 }
